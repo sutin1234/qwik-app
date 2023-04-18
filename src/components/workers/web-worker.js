c7b9456d4535c6c189661435c27e1884d138f611
sutin <injitt>
2023-04-18 16:03:27 +0700

    fix: web worker

--- src/components/workers/web-worker.js
+++ src/components/workers/web-worker.js
@@ -2,18 +2,19 @@ const range = (size = 100, startAt = 0) => {
   return [...Array(size).keys()].map((i) => i + startAt);
 };
 
-self.onmessage = () => {
+self.onmessage = (msg) => {
+  const numRows = msg.data.numRows;
   let data = [];
-  for (const i in range(200, 1)) {
+  for (const i in range(numRows, 1)) {
     data.push({
-      index: i,
+      index: i + 1,
       firstName: "Sutin",
       lastName: "Injitt",
       age: "30",
       sex: "Male",
       tall: "165",
     });
-    console.log("web worker push " + Number(i + 1));
+    console.log("web worker push " + Number(i + 1) + " rows");
   }
   self.postMessage(data);
 };
--- src/routes/web-worker/index.tsx
+++ src/routes/web-worker/index.tsx
@@ -1,9 +1,11 @@
-import { $, component$, useStore } from '@builder.io/qwik';
+import { $, component$, useRef, useStore } from '@builder.io/qwik';
+
+import { ButtonSlot } from "~/components/Buttons/ButtonSlot";
 import { DocumentHead } from '@builder.io/qwik-city';
-import { ButtonSlot } from '../../components/Buttons/ButtonSlot';
 
 export default component$(() => {
-    const store = useStore({ dataList: [], loading: false });
+    const store = useStore({ dataList: [], loading: false, numRows: 100 });
+    const inputRef = useRef<HTMLInputElement>(null)
 
     const increase = $(() => {
         store.loading = true
@@ -11,7 +13,7 @@ export default component$(() => {
         if (window.Worker) {
             const workerScriptURL = new URL('./components/workers/web-worker.js', import.meta.url)
             const worker = new Worker(workerScriptURL)
-            worker.postMessage(store.dataList)
+            worker.postMessage({ numRows: store.numRows })
 
             worker.onmessage = (evt: MessageEvent<any>) => {
                 store.loading = false
@@ -21,13 +23,23 @@ export default component$(() => {
 
     })
 
+    const updateNumRows = $(() => store.numRows = Number(inputRef.current?.value))
+
     return (
         <div>
-            <h1>Qwik Web Workers generate tables in {store.dataList.length} rows</h1>
+            <h1>Qwik Web Workers generate tables in {store.numRows} rows</h1>
+            <div>
+                <label>numRows:</label>
+                <input type="number" class="p-1 border-3" ref={inputRef} onChange$={updateNumRows} value={store.numRows}/> rows.
+            </div>
+            
             {store.loading && store.dataList.length <= 0 ?
                 <div>Loading Data....</div> :
-                <div>
-                    dataList: {JSON.stringify(store.dataList)}
+                <div class="h-4/6 overflow-scroll border-3">
+                    <ul>
+                        {store.dataList.map(m => <li>{JSON.stringify(m)}</li>)}
+                    </ul>
+                    {/* dataList: {JSON.stringify(store.dataList)}  */}
                 </div>
             }
             <ButtonSlot onClick$={increase}>
